9ca3f377292677a40a7d3906cf8d75156b219459
paulgrammer <paulmugaya@live.com>
2021-12-07 18:56:35 +0300

    Added cors

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
 
 var campaignRouter = require('./routes/CampaignRoutes');
 var usersRouter = require('./routes/UserRoutes');
 
 var app = express();
 
+// enable cors
+var corsOptions = {
+    exposedHeaders: ["X-Access-Token"],
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+};
+
+app.use(cors(corsOptions)); 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
